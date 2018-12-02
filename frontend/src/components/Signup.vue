b70a4b4202914bc07f877d96d1c37899cd82fa93
ZeevGerstner <zeev.gerstner@gmail.com>
2018-12-02 09:32:54 +0200

    pull master

--- frontend/src/components/Signup.vue
+++ frontend/src/components/Signup.vue
@@ -1,35 +1,42 @@
 <template>
-    <section>
-        <h2>Signup</h2>
-        <form @submit.prevent="signupUser">
-            <input placeholder="User Name" v-model="newUser.name" type="text">
-            <input placeholder="Password" v-model="newUser.pass" type="password">
-            <button>Signup</button>
-        </form>
-    </section>
+  <section>
+    <h2>Signup</h2>
+    <form @submit.prevent="signupUser">
+      <input
+        placeholder="User Name"
+        v-model="newUser.name"
+        type="text"
+      >
+      <input
+        placeholder="Password"
+        v-model="newUser.pass"
+        type="password"
+      >
+      <button>Signup</button>
+    </form>
+  </section>
 
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            newUser: {
-                name:'',
-                pass:''
-            }
-        }
-    },
-    methods:{
-        signupUser(){
-            console.log(this.newUser)
-            this.newUser = {name:'', pass:''}
-        }
+  data () {
+    return {
+      newUser: {
+        name: '',
+        pass: ''
+      }
     }
+  },
+  methods: {
+    signupUser () {
+      console.log(this.newUser)
+      this.newUser = { name: '', pass: '' }
+    }
+  }
 
 }
 </script>
 
 <style>
-
 </style>
