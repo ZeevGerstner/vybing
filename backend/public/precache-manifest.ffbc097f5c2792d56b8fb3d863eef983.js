7351037daa8f40ac420e50c0b2af340708408abd
boogeh <wagner.amit@gmail.com>
2018-12-06 11:12:30 +0200

    new build + favicon

---
+++ backend/public/precache-manifest.ffbc097f5c2792d56b8fb3d863eef983.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "89e2666c24d37055bcb60e9d2d9f7e35",
+    "url": "/fonts/Roboto-Thin.89e2666c.ttf"
+  },
+  {
+    "revision": "dd1c1706c6a696c9f354754c1894b3ce",
+    "url": "/vybingfav.ico"
+  },
+  {
+    "revision": "fb4c7f5813a0ec14f77da2fb523e3516",
+    "url": "/fonts/Ubuntu-Medium.fb4c7f58.ttf"
+  },
+  {
+    "revision": "e8c49890cf74517ab171",
+    "url": "/js/app.f517ec1d.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "53611f8009f44867b574",
+    "url": "/js/chunk-vendors.b2f2640b.js"
+  },
+  {
+    "revision": "c5e21943d0cf3378a7c2a7682ccafb99",
+    "url": "/img/home-img2.c5e21943.png"
+  },
+  {
+    "revision": "6258ebe97852fd4411b2",
+    "url": "/js/about.dbc2d5ba.js"
+  },
+  {
+    "revision": "bda6448a1a9a4d4771e9b66e43372911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d0fe55f8f5f9d553db316567ee87cc3",
+    "url": "/img/signup-img.3d0fe55f.jpg"
+  },
+  {
+    "revision": "8f385ee775587b351bf6be29575923cc",
+    "url": "/img/logo2.8f385ee7.png"
+  },
+  {
+    "revision": "182620fee990892b0a01ad29c2877d74",
+    "url": "/img/home-img.182620fe.jpg"
+  },
+  {
+    "revision": "3e1af3ef546b9e6ecef9f3ba197bf7d2",
+    "url": "/fonts/Roboto-Regular.3e1af3ef.ttf"
+  },
+  {
+    "revision": "d08840599e05db7345652d3d417574a9",
+    "url": "/fonts/Roboto-Medium.d0884059.ttf"
+  },
+  {
+    "revision": "ee7b96fa85d8fdb8c126409326ac2d2b",
+    "url": "/fonts/Roboto-Bold.ee7b96fa.ttf"
+  },
+  {
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493",
+    "url": "/favicon2.ico"
+  },
+  {
+    "revision": "e8c49890cf74517ab171",
+    "url": "/css/app.233fd24c.css"
+  }
+];
